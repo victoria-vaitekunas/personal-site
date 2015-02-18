cb8175be76699f68bd5b8481e11b81b160d46371
victoria-vaitekunas <victoria.vaitekunas@gmail.com>
2015-02-18 18:30:11 -0500

    modified description

--- archive.php
+++ archive.php
@@ -3,7 +3,7 @@
 		<div class="blogwrap clearfix">
 		<?php single_cat_title( '', false);  ?>
 		<?php if ( category_description() ) : ?>
-			<?php echo category_description(); ?>
+			<?php echo '<div class="cat-descrip">'.category_description().'</div>'; ?>
 			<?php endif; ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 			<div class="blogpost">
@@ -17,7 +17,6 @@
 				<div class="paralellogramlink">
 					<p><a href="<?php echo get_permalink(); ?>">Read more</a></p>
 				</div>
-				<?php make_category_links(); ?>
 			</div><!-- /.blogpost -->
 		<?php endwhile; ?>
 		<?php wp_pagination(); ?>
